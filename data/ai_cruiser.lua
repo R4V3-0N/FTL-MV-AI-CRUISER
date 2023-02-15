21b1fb8f09187584c216e53a518e0e4de7b88860
ChronoVortex <nick@hudsucker.com>
2023-02-14 18:02:49 -0800

    holos heal 25 on jump

---
+++ data/ai_cruiser.lua
@@ -0,0 +1,20 @@
+if not mods or not mods.vertexutil then
+    error("Couldn't find Vertex Tags and Utility Functions! Make sure it's above mods which depend on it in the Slipstream load order", 2)
+end
+
+local vter = mods.vertexutil.vter
+
+local wasJumping = false
+script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
+    local isJumping = false
+    if pcall(function() isJumping = Hyperspace.ships.player.bJumping end) then
+        if not isJumping and wasJumping then
+            for crew in vter(Hyperspace.ships.player.vCrewList) do
+                if crew:GetSpecies() == "rvs_ai_hologram" then
+                    crew:DirectModifyHealth(25.0)
+                end
+            end
+        end
+        wasJumping = isJumping
+    end
+end)
--- data/check_for_vertex.lua
+++
@@ -1,3 +0,0 @@
-if not mods or not mods.vertexutil then
-    error("Couldn't find Vertex Tags and Utility Functions! Make sure it's above mods which depend on it in the Slipstream load order", 2)
-end
