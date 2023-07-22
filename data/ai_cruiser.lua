2efc8f652b383afb24bcd9a564b926b6983bf91b
ChronoVortex <nick@hudsucker.com>
2023-07-21 19:53:26 -0700

    re-implemented intercept drone code

--- data/ai_cruiser.lua
+++ data/ai_cruiser.lua
@@ -6,6 +6,7 @@ if not mods or not mods.inferno then
 end
 
 local vter = mods.vertexutil.vter
+local userdata_table = mods.vertexutil.userdata_table
 
 local function bool_to_num(bool)
     if bool then return 1 end
@@ -133,6 +134,56 @@ function(ShipManager, Projectile, Location, Damage, newTile, beamHit)
     end
   end 
   return Defines.Chain.CONTINUE, beamHit 
+end)
+
+-- Make a drone target enemy drones first before normal anti ship targetting
+-- CV: I wrote this while I felt like crap and my brain was working at 0.1% capacity, god help whoever has to read it later
+local function set_drone_destination(drone)
+    local target = userdata_table(drone, "mods.ai.droneTarget").target
+    if target then
+        local shieldCenter = Hyperspace.Global.GetInstance():GetShipManager(drone.currentSpace).ship:GetBaseEllipse().center
+        drone.destinationLocation.x = (target.currentLocation.x - shieldCenter.x)*1.15 + shieldCenter.x
+        drone.destinationLocation.y = (target.currentLocation.y - shieldCenter.y)*1.15 + shieldCenter.y
+    end
+end
+local function aquire_drone_target(ship, droneName)
+    local otherShip = Hyperspace.Global.GetInstance():GetShipManager(1 - ship.iShipId)
+    if otherShip then
+        local target = nil
+        for drone in vter(otherShip.spaceDrones) do
+            if drone.deployed and drone.currentSpace == otherShip.iShipId and not drone.explosion.tracker.running and drone.blueprint.typeName ~= "SHIP_REPAIR" and drone.blueprint.typeName ~= "HACKING" then
+                target = drone
+                break
+            end
+        end
+        for drone in vter(ship.spaceDrones) do
+            if drone.blueprint.name == droneName then
+                local targetTable = userdata_table(drone, "mods.ai.droneTarget")
+                targetTable.target = target
+                if target then
+                    drone.targetLocation.x = target.currentLocation.x
+                    drone.targetLocation.y = target.currentLocation.y
+                    targetTable.targetingDrone = true
+                elseif targetTable.targetingDrone then
+                    drone.targetLocation = otherShip:GetRandomRoomCenter()
+                    targetTable.targetingDrone = false
+                end
+                if drone.powered and not targetTable.poweredLastFrame then
+                    set_drone_destination(drone)
+                end
+                targetTable.poweredLastFrame = drone.powered
+            end
+        end
+    end
+end
+script.on_internal_event(Defines.InternalEvents.DRONE_FIRE, function(projectile, drone)
+    if drone.blueprint.name == "RVS_AI_MICROFIGHTER_INTERCEPTER" then
+        set_drone_destination(drone)
+    end
+end)
+script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
+    if Hyperspace.ships.player then aquire_drone_target(Hyperspace.ships.player, "RVS_AI_MICROFIGHTER_INTERCEPTER") end
+    if Hyperspace.ships.enemy then aquire_drone_target(Hyperspace.ships.enemy, "RVS_AI_MICROFIGHTER_INTERCEPTER") end
 end)
 
 --Make a drone target systems only
