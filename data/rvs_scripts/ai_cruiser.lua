b40854cc8e64d67314d3e5890c57299dc596f6d1
ChronoVortex <nick@hudsucker.com>
2023-08-13 10:21:25 -0700

    fix holo revive for event deaths

--- data/rvs_scripts/ai_cruiser.lua
+++ data/rvs_scripts/ai_cruiser.lua
@@ -14,11 +14,49 @@ local function bool_to_num(bool)
     return bool and 1 or 0
 end
 
+local function get_crew_count_name(ship, speciesName)
+    if ship then
+        local count = 0
+        for crew in vter(ship.vCrewList) do
+            if crew.iShipId == ship.iShipId and crew:GetSpecies() == speciesName then
+                count = count + 1
+            end
+        end
+        local otherShip = Hyperspace.ships(1 - ship.iShipId)
+        if otherShip then
+            for crew in vter(otherShip.vCrewList) do
+                if crew.iShipId == ship.iShipId and crew:GetSpecies() == speciesName then
+                    count = count + 1
+                end
+            end
+        end
+        return count
+    end
+    return 0
+end
+
 -- Track whether we're in a nebula or a nebula with an ion storm
 script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
     local space = Hyperspace.Global.GetInstance():GetCApp().world.space
     Hyperspace.playerVariables.loc_nebula_nostorm = bool_to_num(space.bNebula and not space.bStorm)
     Hyperspace.playerVariables.loc_nebula_storm = bool_to_num(space.bStorm)
+end)
+
+-- Track holo count
+script.on_internal_event(Defines.InternalEvents.ON_TICK, function()
+    local playerShip = Hyperspace.ships.player
+    if playerShip then
+        local vars = Hyperspace.playerVariables
+        if playerShip:HasEquipment("RVS_REQ_HOLO_TRACK_DEATH") > 0 then
+            local holoCount = get_crew_count_name(playerShip, "rvs_ai_hologram")
+            if vars.loc_holos_last_count > -1 and vars.loc_holos_last_count > holoCount then
+                vars.loc_holos_dead = vars.loc_holos_dead + vars.loc_holos_last_count - holoCount
+            end
+            vars.loc_holos_last_count = holoCount
+        else
+            vars.loc_holos_last_count = -1
+        end
+    end
 end)
 
 -- Heal holograms on jump
