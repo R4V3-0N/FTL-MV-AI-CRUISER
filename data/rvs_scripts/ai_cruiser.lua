113046a7285ddc51d5d3d680ed3888e63141750b
ChronoVortex <nick@hudsucker.com>
2023-08-29 14:18:40 -0700

    swarm aug works on drones destroyed in player space

--- data/rvs_scripts/ai_cruiser.lua
+++ data/rvs_scripts/ai_cruiser.lua
@@ -457,16 +457,34 @@ end)
 --RVS_AI_SWARM implementation (CHANGE NAME LATER)
 
 --Spawn drone when drone is destroyed
+local swarmChildProj = Hyperspace.Global.GetInstance():GetBlueprints():GetWeaponBlueprint("RVS_AI_MICROFIGHTER_SWARM_CHILD_PROJ_DUMMY")
 script.on_internal_event(Defines.InternalEvents.DRONE_COLLISION,
 function(Drone, Projectile, Damage, CollisionResponse)
     local thisShip = Hyperspace.ships(Drone.iShipId)
     local otherShip = Hyperspace.ships(1 - Drone.iShipId)
-    if thisShip and otherShip and thisShip.iShipId == Drone.currentSpace and Damage.iIonDamage <= 0 and Damage.iDamage > 0 and otherShip:HasAugmentation("RVS_AI_SWARM") > 0 then --If drone would be destroyed and ship has drone spawning aug
-        --Spawn drone at drone's location, location shouldn't really matter if destroyed drone is a defense drone because it's in a different space anyway
-        spawn_temp_drone{name = "RVS_AI_MICROFIGHTER_SWARM_CHILD", ownerShip = otherShip, targetShip = thisShip, position = Drone.currentLocation} 
+    if thisShip and otherShip and Damage.iIonDamage <= 0 and Damage.iDamage > 0 and otherShip:HasAugmentation("RVS_AI_SWARM") > 0 then --If drone would be destroyed and ship has drone spawning aug
+        --Spawn drone at drone's location, or if the drone is in the other ship's space, spawn a projectile that will become the drone
+        if thisShip.iShipId == Drone.currentSpace then
+            spawn_temp_drone{name = "RVS_AI_MICROFIGHTER_SWARM_CHILD", ownerShip = otherShip, targetShip = thisShip, position = Drone.currentLocation}
+        else
+            local spaceManager = Hyperspace.Global.GetInstance():GetCApp().world.space
+            local shipCenter = otherShip._targetable:GetShieldShape().center
+            local heading = math.deg(math.atan(Drone.currentLocation.y - shipCenter.y, Drone.currentLocation.x - shipCenter.x))
+            spaceManager:CreateMissile(swarmChildProj, Drone.currentLocation, Drone.currentSpace, Projectile.ownerId, thisShip:GetRandomRoomCenter(), Drone.iShipId, heading)
+        end
     end
     return Defines.Chain.CONTINUE
-end) 
+end)
+script.on_internal_event(Defines.InternalEvents.PROJECTILE_PRE, function(projectile)
+    if projectile.extend.name == "RVS_AI_MICROFIGHTER_SWARM_CHILD_PROJ_DUMMY" and projectile.ownerId ~= projectile.currentSpace then
+        local ship = Hyperspace.ships(projectile.ownerId)
+        local otherShip = Hyperspace.ships(1 - projectile.ownerId)
+        if ship and otherShip then
+            spawn_temp_drone{name = "RVS_AI_MICROFIGHTER_SWARM_CHILD", ownerShip = ship, targetShip = otherShip, position = projectile.position}
+        end
+        projectile:Kill()
+    end
+end)
 
 
 --V: Should this trigger on resist? Leaving like this for now unless told otherwise.
