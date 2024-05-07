76cda0b34ca49584ed80847d978f749db709715b
R4V3-0N <r4v30nonline@gmail.com>
2024-05-07 18:31:36 +1200

    Anti-Grav Engine stuff (thx Arc)

--- data/rvs_scripts/ai_cruiser.lua
+++ data/rvs_scripts/ai_cruiser.lua
@@ -435,6 +435,44 @@ limitShots.RVS_AI_MISSILE_1 = 7
 limitShots.RVS_AI_MISSILE_2 = 5
 limitShots.RVS_AI_MISSILE_3 = 15
 
+-- Anti Gravity Engines
+script.on_internal_event(Defines.InternalEvents.GET_DODGE_FACTOR, function(shipManager, value)
+    if shipManager:HasAugmentation("RVS_ANTI_GRAVITY_ENGINE") > 0 then
+        local dodgeTable = userdata_table(shipManager, "mods.ai.grav_engine")
+        local valueAdd = 0
+        if dodgeTable.addDodge then
+            valueAdd = math.Round(dodgeTable.addDodge, 0)
+        end
+        value = math.max(value - 5 + valueAdd, 0)
+    end
+    return Defines.Chain.CONTINUE, value
+end)
+
+script.on_internal_event(Defines.InternalEvents.SHIELD_COLLISION, function(shipManager, projectile, damage, response)
+    if shipManager:HasAugmentation("RVS_ANTI_GRAVITY_ENGINE") > 0 then
+        if response.damage > 0 or response.superDamage > 0 then
+            local dodgeTable = userdata_table(shipManager, "mods.ai.grav_engine")
+            if dodgeTable.addDodge then
+                dodgeTable.addDodge = math.min(dodgeTable.addDodge + 5, 40)
+            else
+                dodgeTable.addDodge = 5
+            end
+        end
+    end
+end)
+
+script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(shipManager)
+    if shipManager:HasAugmentation("RVS_ANTI_GRAVITY_ENGINE") > 0 then
+        local dodgeTable = userdata_table(shipManager, "mods.ai.grav_engine")
+        if dodgeTable.addDodge then
+            dodgeTable.addDodge = math.max(dodgeTable.addDodge - Hyperspace.FPS.SpeedFactor/16, 0)
+            if dodgeTable.addDodge == 0 then
+                dodgeTable.addDodge = nil
+            end
+        end
+    end
+end)
+
 
 --Implementation uses lifespan for save and load, replace with table implementation when saving features are available
 --I think lifespan is only used for surge drones so it's ok to use here
