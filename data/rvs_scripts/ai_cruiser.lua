666ac6ee4fc062036c52439697af40429f95714a
R4V3-0N <r4v30nonline@gmail.com>
2024-05-11 22:03:41 +1200

    Fixed Splitter Beam colour error

--- data/rvs_scripts/ai_cruiser.lua
+++ data/rvs_scripts/ai_cruiser.lua
@@ -132,9 +132,9 @@ local burstsToBeams = {}
 burstsToBeams.RVS_BEAM_SHOTGUN_1 = pinpoint1
 burstsToBeams.RVS_BEAM_SHOTGUN_2 = pinpoint1
 burstsToBeams.RVS_BEAM_SHOTGUN_3 = pinpoint2
-burstsToBeams.RVS_BEAM_SHOTGUN_1 = fm_epinpoint1
-burstsToBeams.RVS_BEAM_SHOTGUN_2 = fm_epinpoint1
-burstsToBeams.RVS_BEAM_SHOTGUN_3 = fm_epinpoint2
+burstsToBeams.FM_RVS_SPLITTER_ENERGY_1 = fm_epinpoint1
+burstsToBeams.FM_RVS_SPLITTER_ENERGY_2 = fm_epinpoint1
+burstsToBeams.FM_RVS_SPLITTER_ENERGY_3 = fm_epinpoint2
 script.on_internal_event(Defines.InternalEvents.PROJECTILE_FIRE, function(projectile, weapon)
     local beamReplacement = burstsToBeams[weapon.blueprint.name]
     if beamReplacement then
