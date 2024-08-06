1cd221b2c927c89a736508812488b53f5c82994c
ChronoVortex <33610911+ChronoVortex@users.noreply.github.com>
2024-08-06 01:59:44 -0700

    update pulsar nebula upgrades

--- data/rvs_scripts/ai_cruiser.lua
+++ data/rvs_scripts/ai_cruiser.lua
@@ -735,10 +735,18 @@ function(projectile, weapon)
     end
 end)
 
+--Power Upgrade
+script.on_internal_event(Defines.InternalEvents.SET_BONUS_POWER, function(system, amount)
+    if system:GetId() == Hyperspace.playerVariables.loc_ai_bonus_power_system then
+        amount = amount + Hyperspace.ships(system._shipObj.iShipId):GetAugmentationValue("RVS_SCIENCE_ION_POWER")
+    end
+    return Defines.Chain.CONTINUE, amount
+end)
+
 --Evasion Armor
 script.on_internal_event(Defines.InternalEvents.GET_DODGE_FACTOR,
 function(ShipManager, dodge)
-    dodge = dodge + ShipManager:GetAugmentationValue("RVS_DODGE_ARMOR")
+    dodge = dodge + ShipManager:GetAugmentationValue("RVS_SCIENCE_ION_DODGE")
     return Defines.Chain.CONTINUE, dodge
 end)
 
